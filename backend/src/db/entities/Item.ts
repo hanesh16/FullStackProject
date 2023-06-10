6d4aab467a90c32f63509e525199bb3ac3bf5b17
Expert <247solutionsexpert@gmail.com>
2023-06-10 04:07:19 -0400

    1. Updated MantToOne entity in item
    Signed-off-by: Expert <247solutionsexpert@gmail.com>

--- backend/src/db/entities/Item.ts
+++ backend/src/db/entities/Item.ts
@@ -1,5 +1,6 @@
-import {Entity, ManyToOne, Property} from "@mikro-orm/core";
+import {Entity, ManyToOne, OneToMany, Property} from "@mikro-orm/core";
 import {SoftDeletable} from "mikro-orm-soft-delete";
+import {ItemOwner} from "./ItemOwner.js";
 // removed import for DoggerBaseEntity file
 import {User} from "./User.js";
 
@@ -9,8 +10,11 @@ export class Item {
     // The person who will add the Item
     @ManyToOne({ primary: true })
     owner!: User;
-
-    @Property()
+    
+    //The person who will sell the Item
+    @ManyToOne(() => ItemOwner)
+    author!: ItemOwner;
+    
     created_at = new Date();
 
     @Property({ nullable: true })
